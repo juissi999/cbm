8967ea8919a62cec1ac541bd4cbed7f5325671ad
juissi999 <jussi.kaatiala@gmail.com>
2020-05-14 15:28:19 +0300

    ControlBar buttons with sample functionality added

--- src/components/ControlBar.vue
+++ src/components/ControlBar.vue
@@ -1,13 +1,21 @@
-<template
-  ><div>
-    controlbar
+<template>
+  <div>
+    <button v-on:click="images -= 1">-</button>
+    Images: {{ images }}
+    <button v-on:click="images += 1">+</button>
+    <button v-on:click="size -= 1">-</button>
+    Imagesize: {{ size }}
+    <button v-on:click="size += 1">+</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ControlBar',
-  props: []
+  props: [],
+  data() {
+    return { images: 0, size: 100 }
+  }
 }
 </script>
 
