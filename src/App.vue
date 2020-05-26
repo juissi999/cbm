8b7db44ff9a2b43927b98dc5ce0acfb71f846d0f
juissi999 <jussi.kaatiala@gmail.com>
2020-05-26 22:50:39 +0300

    Add h1-tags and remove unnecessary styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    {{ header }}
+    <h1>
+      {{ header }}
+    </h1>
     <ControlBar
       v-bind:count="count"
       v-bind:imgsize="height"
@@ -58,13 +60,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
