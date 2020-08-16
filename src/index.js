eb32154d6791d16f4cd989584435441466b9400e
juissi999 <jussi.kaatiala@gmail.com>
2020-08-16 10:59:24 +0300

    Remove old comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import Vue from 'vue/dist/vue.runtime'
 import App from './App.vue'
 
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// })
-
 new Vue({
-  render:h=>h(App),
+  render: h => h(App)
 }).$mount('#app')
